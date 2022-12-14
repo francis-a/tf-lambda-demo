8b8da8009ab69c076a442d906406c957b85052ca
Francis Altomare <altomare.francis@gmail.com>
2022-12-14 13:44:40 -0500

    fix format

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -31,9 +31,9 @@ resource "aws_iam_role" "api_lambda_execution_role" {
 }
 
 resource "aws_iam_role_policy" "api_lambda_policy" {
-  role   = aws_iam_role.api_lambda_execution_role.id
+  role = aws_iam_role.api_lambda_execution_role.id
   policy = jsonencode({
-    Version   = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
       {
         Effect : "Allow"
