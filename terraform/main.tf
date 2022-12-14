914ba15cb8dc96d28cbbe6b872a0ce5e6aa116ae
Francis Altomare <altomare.francis@gmail.com>
2022-12-14 07:34:30 -0500

    Added TF plugin

---
+++ terraform/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  backend "s3" {
+    key            = "tf/${var.stack_name}"
+    region         = "us-east-1"
+    bucket         = "${var.stack_name}-terraform-state"
+    dynamodb_table = "${var.stack_name}-terraform-state"
+    encrypt        = false
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.33.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  #  AWS profile to run this under
+  profile = var.aws_profile
+  #  Tags are applied to all resources in the stack
+  #  These tags are used
+  default_tags {
+    tags = {
+      Service     = var.stack_name
+      Environment = var.stack_environment
+    }
+  }
+}
+
+locals {
+  name = "${var.stack_name}-${var.stack_environment}"
+}
+
+## Resource group for organization
+resource "aws_resourcegroups_group" "lambda_demo" {
+  name = local.name
+  resource_query {
+    query = jsonencode({
+      ResourceTypeFilters = ["AWS::AllSupported"]
+      TagFilters          = [
+        {
+          Key    = "Service"
+          Values = [var.stack_name]
+        },
+        {
+          Key    = "Environment"
+          Values = [var.stack_environment]
+        }
+      ]
+    })
+  }
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "stack_name" {
+  default = "tf-lambda-demo"
+}
+variable "stack_environment" {
+  default = "dev"
+}
+variable "aws_profile" {
+  default = "default"
+}
